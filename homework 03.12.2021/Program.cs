75addc9794104e988374acedbab940c28383de45
[Sadigkhan Gaykhanov] <[sadigkhaneg@code.edu.az]>
2021-12-03 02:17:00 +0400

    Add project files.

---
+++ homework 03.12.2021/Program.cs
@@ -0,0 +1,17 @@
+﻿{
+    Employee maas = new Employee();
+    maas.Maas("Sadigkhan", "Gaykhanov", "Developer olmaga calisir", 30);
+}
+class Employee
+{
+    public string Ad;
+    public string Soyad;
+    public string Vezife;
+    public int GunlukMaas;
+    public int AyliqMaas;
+    public void Maas(string Ad,string Soyad,string Vezife,int GunlukMaas)
+    {
+        AyliqMaas = GunlukMaas * 30;
+        Console.WriteLine($"{Ad} {Soyad} {Vezife} {AyliqMaas}");
+    }
+}
